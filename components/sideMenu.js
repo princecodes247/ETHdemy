2059e5a39777f6ed9fd2e748fb9e550a154716e7
PrinceCodes <princecodes247@gmail.com>
2021-10-19 09:36:50 +0100

    feat: create side menu component

---
+++ components/sideMenu.js
@@ -0,0 +1,68 @@
+
+//   <li class="menu__item active">
+//   <a href="./dashboard.html" target="_blank" rel="noopener noreferrer">
+//       <div class="menu__icon">
+//       <ion-icon name="wallet-outline"></ion-icon>
+//   </div>
+//       <p>Dashboard</p>
+//   </a>
+// </li>
+// <li class="menu__item">
+//   <a href="./market.html" target="_blank" rel="noopener noreferrer">
+//       <div class="menu__icon">
+//       <ion-icon name="stats-chart-outline"></ion-icon>
+//   </div>
+//       <p>Market</p>
+//   </a>
+// </li>
+// <li class="menu__item">
+//   <a href="./payment.html" target="_blank" rel="noopener noreferrer">
+//       <div class="menu__icon">
+//       <ion-icon name="card-outline"></ion-icon>
+//   </div>
+//       <p>Deposit/Withdrawal</p>
+//   </a>
+// </li>
+// <li class="menu__item">
+//   <a href="./history.html" target="_blank" rel="noopener noreferrer">
+//       <div class="menu__icon">
+//       <ion-icon name="calendar-outline"></ion-icon>
+//   </div>
+//       <p>Transactions History</p>
+//   </a>
+// </li>
+// <li class="menu__item">
+//   <a href="./accounts.html" target="_blank" rel="noopener noreferrer">
+//       <div class="menu__icon">
+//           <ion-icon name="business-outline"></ion-icon>
+//   </div>
+//       <p>Accounts</p>
+//   </a>
+// </li>
+// <li class="menu__item">
+//   <a href="./profile.html" target="_blank" rel="noopener noreferrer">
+//       <div class="menu__icon">
+//       <ion-icon name="settings-outline"></ion-icon>
+//   </div>
+//       <p>Settings</p>
+//   </a>
+// </li>
+
+import styles from '../styles/sideMenu.module.css'
+import SideMenuItem from './sideMenuItem'
+
+export default function SideMenu({ children }) {
+    return <aside className={styles.menuContainer}>
+            <ul className={styles.menu}>
+              <SideMenuItem name="Dashboard" icon="wallet"></SideMenuItem>
+              <SideMenuItem name="Market" icon="wallet"></SideMenuItem>
+              <SideMenuItem name="Deposit/Withdraw" icon="wallet"></SideMenuItem>
+              <SideMenuItem name="Transaction History" icon="wallet"></SideMenuItem>
+              <SideMenuItem name="Add Accounts" icon="wallet"></SideMenuItem>
+              <SideMenuItem name="Settings" icon="wallet"></SideMenuItem>
+            {children}
+            </ul>
+        </aside>
+    
+  }
+  
