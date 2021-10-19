cb583e2659bbc0a2b8bf605a8f1cea239b9b064f
PrinceCodes <princecodes247@gmail.com>
2021-10-19 21:03:07 +0100

    refactor: rename dashboard to index

--- pages/user/[id]/index.js
+++ pages/user/[id]/index.js
@@ -2,13 +2,20 @@ import Link from 'next/link'
 import Head from 'next/head'
 import DashboardLayout from '../../../components/dashboardLayout'
 
+import { useRouter } from 'next/router'
+  
+
 export default function Dashboard() {
+  const router = useRouter()
+  const url = router.asPath
+
+
   return (
     <DashboardLayout>
       <Head>
         <title>Dashboard</title>
       </Head>
-      <h1>First Post</h1>
+      <h1>First </h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
