96b1ecae683eb7242f30ef2ac90f37698f788b7c
PrinceCodes <princecodes247@gmail.com>
2021-10-20 13:22:33 +0100

    fix: update dashboardSection styling

--- components/dashboardSection.js
+++ components/dashboardSection.js
@@ -1,9 +1,9 @@
 import styles from '../styles/dashboardLayout.module.css'
 
-export default function DashboardSection(props, { children }) {
-    return  <section className="sect sect--income_summary">
+export default function DashboardSection(props) {
+    return  <section className={styles.sect,styles[`sect_${props.sect}`]}>
     <h3>{props.name}</h3>
-    {children}
+    {props.children}
 </section>
   }
   
