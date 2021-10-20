e0c5212df72890dd3b74a7088b86a8d3155e7f04
PrinceCodes <princecodes247@gmail.com>
2021-10-20 09:09:48 +0100

    feat: create Dashboard Graph Component

---
+++ components/dashboardGraphSection.js
@@ -0,0 +1,13 @@
+import styles from '../styles/dashboardLayout.module.css'
+
+import DashboardSection from './dashboardSection'
+
+export default function DashboardGraphSection({ children }) {
+    return <DashboardSection>
+        <div className="graph">
+
+        </div>
+    </DashboardSection>
+      
+  }
+  
