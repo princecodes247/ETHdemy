4bd59dadcabf49473b48f52964af79164cbca281
PrinceCodes <princecodes247@gmail.com>
2021-10-20 09:11:30 +0100

    feat: implement Dashboard Section Component

--- pages/user/[id]/index.js
+++ pages/user/[id]/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import DashboardLayout from '../../../components/dashboardLayout'
+import DashboardSection from '../../../components/dashboardSection'
 
 import { useRouter } from 'next/router'
   
@@ -16,6 +17,28 @@ export default function Dashboard() {
         <title>Dashboard</title>
       </Head>
       <h1>First </h1>
+      <DashboardSection name="Wallet Details">
+          <div className="summary">
+               <div className="summary__details">
+                <p>Investments: $1399</p>
+                <p>Today{`'`}s Profit: $1399</p>
+               </div>
+                <div className="summary__balance">
+                    <p>Your Balance is </p>
+                    <p className="big">$140</p>
+                </div>
+            </div>
+      </DashboardSection>
+      <DashboardSection name="">
+          <div className="graph">
+               
+            </div>
+      </DashboardSection>
+      <DashboardSection name="Top 10 Ranking">
+          
+          
+
+      </DashboardSection>
       <h2>
         <Link href="/">
           <a>Back to home</a>
