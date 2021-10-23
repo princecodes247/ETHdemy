dbee4f068d2f7eb3e02be836df5d61c8bc38f16f
PrinceCodes <princecodes247@gmail.com>
2021-10-23 08:25:29 +0100

    feat: create _document file

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
