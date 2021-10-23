2a51f1271328bb9f0ad71ec198a9c24c713d9db7
PrinceCodes <princecodes247@gmail.com>
2021-10-23 08:16:13 +0100

    feat: implement Account Table

--- pages/user/[id]/accounts.js
+++ pages/user/[id]/accounts.js
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 import Head from 'next/head'
+
 import DashboardLayout from '../../../components/dashboardLayout'
+import DashboardSection from '../../../components/dashboardSection'
+import AccountsTable from '../../../components/accountsTable'
 
 import { useRouter } from 'next/router'
   
@@ -15,12 +18,37 @@ export default function Accounts() {
       <Head>
         <title>Dashboard</title>
       </Head>
-      <h1>Accounts </h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <DashboardSection name="Add Accounts">
+      
+      </DashboardSection>
+      <AccountsTable/>
+      <DashboardSection name="Add Bank Account or Crypto Wallet">
+      <div className="w-full max-w-xs">
+  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+        Username
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+        Password
+      </label>
+      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+      <p className="text-red-500 text-xs italic">Please choose a password.</p>
+    </div>
+    <div className="flex items-center justify-between">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+      </button>
+      <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        Forgot Password?
+      </a>
+    </div>
+  </form>
+  </div>
+      </DashboardSection>
     </DashboardLayout>
   )
 }
