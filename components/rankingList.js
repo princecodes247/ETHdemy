c54f92606463ac4a0cbe8c04aa3e31b247b9f722
PrinceCodes <princecodes247@gmail.com>
2021-10-23 08:14:19 +0100

    feat: Create ranking list component

---
+++ components/rankingList.js
@@ -0,0 +1,13 @@
+import styles from '../styles/dashboardLayout.module.css'
+
+import DashboardSection from './dashboardSection'
+
+export default function RankingList(props) {
+    return <DashboardSection name="" sect="analytics">
+        <div className={styles.graph}>
+
+        </div>
+    </DashboardSection>
+      
+  }
+  
